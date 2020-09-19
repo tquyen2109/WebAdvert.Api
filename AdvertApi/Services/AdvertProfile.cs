e8975d31dc260ddbb3a279d9749f91ba68595267
Quyen Tran <t.quyen1997@yahoo.com>
2020-09-18 20:23:04 -0400

    Advert Respone API

---
+++ AdvertApi/Services/AdvertProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AutoMapper;
+namespace AdvertApi.Services
+{
+    public class AdvertProfile : Profile
+    {
+        public AdvertProfile()
+        {
+            CreateMap<AdvertModel, AdvertDbModel>();
+        }
+    }
+}
